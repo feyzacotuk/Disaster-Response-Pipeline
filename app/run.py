f51431156ed28e27debc597644934701804d7367
feyzacotuk <65720780+feyzacotuk@users.noreply.github.com>
2024-07-28 20:38:09 +0300

    Update run.py

--- app/run.py
+++ app/run.py
@@ -5,12 +5,10 @@ import pandas as pd
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
-from flask import Flask
-from flask import render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
-
 
 app = Flask(__name__)
 
@@ -25,26 +23,24 @@ def tokenize(text):
 
     return clean_tokens
 
-# load data
+# Veriyi yükleyin
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponse', engine)
 
-# load model
+# Modeli yükleyin
 model = joblib.load("../models/classifier.pkl")
 
-
-# index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
+    # Görselleştirmeler için gerekli verileri çıkarın
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
+    category_counts = df.iloc[:, 4:].sum().sort_values(ascending=False)
+    category_names = list(category_counts.index)
+    
+    # Görselleştirmeleri oluşturun
     graphs = [
         {
             'data': [
@@ -53,48 +49,58 @@ def index():
                     y=genre_counts
                 )
             ],
-
             'layout': {
-                'title': 'Distribution of Message Genres',
+                'title': 'Mesaj Türlerinin Dağılımı',
                 'yaxis': {
-                    'title': "Count"
+                    'title': "Sayı"
                 },
                 'xaxis': {
-                    'title': "Genre"
+                    'title': "Tür"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+            'layout': {
+                'title': 'Mesaj Kategorilerinin Dağılımı',
+                'yaxis': {
+                    'title': "Sayı"
+                },
+                'xaxis': {
+                    'title': "Kategori"
                 }
             }
         }
     ]
     
-    # encode plotly graphs in JSON
+    # Plotly grafiklerini JSON olarak kodlayın
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
-    # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
-
-# web page that handles user query and displays model results
 @app.route('/go')
 def go():
-    # save user input in query
+    # Kullanıcı girdisini query olarak kaydedin
     query = request.args.get('query', '') 
 
-    # use model to predict classification for query
+    # Modeli kullanarak query'nin sınıflandırmasını tahmin edin
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
         query=query,
         classification_result=classification_results
     )
 
-
 def main():
     app.run(host='0.0.0.0', port=3000, debug=True)
-
 
 if __name__ == '__main__':
     main()
